d8c89632557fc751fa87900f6699c1208be455d0
Will Green <willagr@proton.me>
2026-01-26 11:14:02 +0000

    Add company logo hover effect and SVG assets
    Introduces a hover effect for company logos, transitioning from treated to original colored versions using CSS and updated JSX structure. Adds Beamery and Deloitte SVG logo assets to the public directory and updates the page to use external logo images for color transitions.

--- app/page.tsx
+++ app/page.tsx
@@ -828,19 +828,33 @@ export default function Page() {
           </h2>
           <div className="flex flex-col md:flex-row md:flex-wrap md:items-center md:justify-between gap-8 md:gap-16 w-full">
             {/* Microsoft Logo */}
-            <div className="w-full md:flex-1 md:min-w-[20px] flex items-center justify-center">
-              <svg viewBox="0 0 337.6 72" className="h-10 md:h-12 w-auto md:max-w-[150px]" fill="none">
+            <a 
+              href="https://www.microsoft.com" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="w-full md:flex-1 md:min-w-[20px] flex items-center justify-center company-logo-link"
+            >
+              <div className="logo-container h-10 md:h-12 w-auto md:max-w-[150px]">
+                <svg viewBox="0 0 337.6 72" className="h-10 md:h-12 w-auto md:max-w-[150px] logo-treated" fill="none">
                 <path fill={`${DARK_TEXT}80`} d="M140.4,14.4v43.2h-7.5V23.7h-0.1l-13.4,33.9h-5l-13.7-33.9h-0.1v33.9h-6.9V14.4h10.8l12.4,32h0.2l13.1-32H140.4 z M146.6,17.7c0-1.2,0.4-2.2,1.3-3c0.9-0.8,1.9-1.2,3.1-1.2c1.3,0,2.4,0.4,3.2,1.2s1.3,1.8,1.3,3c0,1.2-0.4,2.2-1.3,3 c-0.9,0.8-1.9,1.2-3.2,1.2s-2.3-0.4-3.1-1.2C147.1,19.8,146.6,18.8,146.6,17.7z M154.7,26.6v31h-7.3v-31H154.7z M176.8,52.3 c1.1,0,2.3-0.2,3.6-0.8c1.3-0.5,2.5-1.2,3.6-2v6.8c-1.2,0.7-2.5,1.2-4,1.5c-1.5,0.3-3.1,0.5-4.9,0.5c-4.6,0-8.3-1.4-11.1-4.3 c-2.9-2.9-4.3-6.6-4.3-11c0-5,1.5-9.1,4.4-12.3c2.9-3.2,7-4.8,12.4-4.8c1.4,0,2.8,0.2,4.1,0.5c1.4,0.3,2.5,0.8,3.3,1.2v7 c-1.1-0.8-2.3-1.5-3.4-1.9c-1.2-0.4-2.4-0.7-3.6-0.7c-2.9,0-5.2,0.9-7,2.8s-2.6,4.4-2.6,7.6c0,3.1,0.9,5.6,2.6,7.3 C171.6,51.4,173.9,52.3,176.8,52.3z M204.7,26.1c0.6,0,1.1,0,1.6,0.1s0.9,0.2,1.2,0.3v7.4c-0.4-0.3-0.9-0.6-1.7-0.8 s-1.6-0.4-2.7-0.4c-1.8,0-3.3,0.8-4.5,2.3s-1.9,3.8-1.9,7v15.6h-7.3v-31h7.3v4.9h0.1c0.7-1.7,1.7-3,3-4 C201.2,26.6,202.8,26.1,204.7,26.1z M207.9,42.6c0-5.1,1.5-9.2,4.3-12.2c2.9-3,6.9-4.5,12-4.5c4.8,0,8.6,1.4,11.3,4.3 s4.1,6.8,4.1,11.7c0,5-1.5,9-4.3,12c-2.9,3-6.8,4.5-11.8,4.5c-4.8,0-8.6-1.4-11.4-4.2C209.3,51.3,207.9,47.4,207.9,42.6z M215.5,42.3c0,3.2,0.7,5.7,2.2,7.4s3.6,2.6,6.3,2.6c2.6,0,4.7-0.8,6.1-2.6c1.4-1.7,2.1-4.2,2.1-7.6c0-3.3-0.7-5.8-2.1-7.6 c-1.4-1.7-3.5-2.6-6-2.6c-2.7,0-4.7,0.9-6.2,2.7C216.2,36.5,215.5,39,215.5,42.3z M250.5,34.8c0,1,0.3,1.9,1,2.5 c0.7,0.6,2.1,1.3,4.4,2.2c2.9,1.2,5,2.5,6.1,3.9c1.2,1.5,1.8,3.2,1.8,5.3c0,2.9-1.1,5.2-3.4,7c-2.2,1.8-5.3,2.6-9.1,2.6 c-1.3,0-2.7-0.2-4.3-0.5c-1.6-0.3-2.9-0.7-4-1.2v-7.2c1.3,0.9,2.8,1.7,4.3,2.2c1.5,0.5,2.9,0.8,4.2,0.8c1.6,0,2.9-0.2,3.6-0.7 c0.8-0.5,1.2-1.2,1.2-2.3c0-1-0.4-1.8-1.2-2.6c-0.8-0.7-2.4-1.5-4.6-2.4c-2.7-1.1-4.6-2.4-5.7-3.8s-1.7-3.2-1.7-5.4 c0-2.8,1.1-5.1,3.3-6.9c2.2-1.8,5.1-2.7,8.6-2.7c1.1,0,2.3,0.1,3.6,0.4s2.5,0.6,3.4,0.9V34c-1-0.6-2.1-1.2-3.4-1.7 c-1.3-0.5-2.6-0.7-3.8-0.7c-1.4,0-2.5,0.3-3.2,0.8C250.9,33.1,250.5,33.8,250.5,34.8z M266.9,42.6c0-5.1,1.5-9.2,4.3-12.2 c2.9-3,6.9-4.5,12-4.5c4.8,0,8.6,1.4,11.3,4.3s4.1,6.8,4.1,11.7c0,5-1.5,9-4.3,12c-2.9,3-6.8,4.5-11.8,4.5c-4.8,0-8.6-1.4-11.4-4.2 C268.4,51.3,266.9,47.4,266.9,42.6z M274.5,42.3c0,3.2,0.7,5.7,2.2,7.4s3.6,2.6,6.3,2.6c2.6,0,4.7-0.8,6.1-2.6 c1.4-1.7,2.1-4.2,2.1-7.6c0-3.3-0.7-5.8-2.1-7.6c-1.4-1.7-3.5-2.6-6-2.6c-2.7,0-4.7,0.9-6.2,2.7C275.3,36.5,274.5,39,274.5,42.3z M322.9,32.6h-10.9v25h-7.4v-25h-5.2v-6h5.2v-4.3c0-3.2,1.1-5.9,3.2-8s4.8-3.1,8.1-3.1c0.9,0,1.7,0.1,2.4,0.1s1.3,0.2,1.8,0.4v6.3 c-0.2-0.1-0.7-0.3-1.3-0.5c-0.6-0.2-1.3-0.3-2.1-0.3c-1.5,0-2.7,0.5-3.5,1.4c-0.8,0.9-1.2,2.4-1.2,4.2v3.7h10.9v-7l7.3-2.2v9.2h7.4 v6h-7.4v14.5c0,1.9,0.4,3.2,1,4c0.7,0.8,1.8,1.2,3.3,1.2c0.4,0,0.9-0.1,1.5-0.3c0.6-0.2,1.1-0.4,1.5-0.7v6c-0.5,0.3-1.2,0.5-2.3,0.7 c-1.1,0.2-2.1,0.3-3.2,0.3c-3.1,0-5.4-0.8-6.9-2.4c-1.5-1.6-2.3-4.1-2.3-7.4L322.9,32.6L322.9,32.6z" />
                 <rect fill={`${DARK_TEXT}80`} width="34.2" height="34.2" />
                 <rect fill={`${DARK_TEXT}80`} x="37.8" width="34.2" height="34.2" />
                 <rect fill={`${DARK_TEXT}80`} y="37.8" width="34.2" height="34.2" />
                 <rect fill={`${DARK_TEXT}80`} x="37.8" y="37.8" width="34.2" height="34.2" />
               </svg>
-            </div>
+              <img src="/microsoft-logo.svg" alt="Microsoft" className="h-10 md:h-12 w-auto md:max-w-[150px] logo-colored" />
+              </div>
+            </a>
 
             {/* Ravelin Logo */}
-            <div className="w-full md:flex-1 md:min-w-[20px] flex items-center justify-center">
-              <svg viewBox="0 0 159.1 45" className="h-10 md:h-12 w-auto md:max-w-[150px]" fill="none">
+            <a 
+              href="https://www.ravelin.com" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="w-full md:flex-1 md:min-w-[20px] flex items-center justify-center company-logo-link"
+            >
+              <div className="logo-container h-10 md:h-12 w-auto md:max-w-[150px]">
+                <svg viewBox="0 0 159.1 45" className="h-10 md:h-12 w-auto md:max-w-[150px] logo-treated" fill="none">
                 <style>{`.st0,.st1{fill:${DARK_TEXT}80}`}</style>
                 <g>
                   <g id="ravelinSvg" transform="translate(49.635 7.122)">
@@ -860,11 +874,19 @@ export default function Page() {
                   </g>
                 </g>
               </svg>
-            </div>
+              <img src="/ravelin-logo.svg" alt="Ravelin" className="h-10 md:h-12 w-auto md:max-w-[150px] logo-colored" />
+              </div>
+            </a>
 
             {/* Deloitte Logo */}
-            <div className="w-full md:flex-1 md:min-w-[20px] flex items-center justify-center">
-              <svg viewBox="13.8 14.75 892.4 170.5" className="h-10 md:h-12 w-auto md:max-w-[150px]" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <a 
+              href="https://www.deloitte.com" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="w-full md:flex-1 md:min-w-[20px] flex items-center justify-center company-logo-link"
+            >
+              <div className="logo-container h-10 md:h-12 w-auto md:max-w-[150px]">
+                <svg viewBox="13.8 14.75 892.4 170.5" className="h-10 md:h-12 w-auto md:max-w-[150px] logo-treated" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g transform="translate(-2521.9698,-654.34261)">
                   <g transform="matrix(3.0676446,0,0,3.0676446,-6165.655,-2195.369)">
                     <g transform="matrix(1.7716542,0,0,1.7716542,-2301.0501,-745.37663)">
@@ -882,11 +904,19 @@ export default function Page() {
                   </g>
                 </g>
               </svg>
-            </div>
+              <img src="/deloitte-logo.svg" alt="Deloitte" className="h-10 md:h-12 w-auto md:max-w-[150px] logo-colored" />
+              </div>
+            </a>
 
             {/* OakNorth Bank Logo */}
-            <div className="w-full md:flex-1 md:min-w-[20px] flex items-center justify-center">
-              <svg viewBox="0 0 190 50" className="h-10 md:h-12 w-auto md:max-w-[150px]" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <a 
+              href="https://www.oaknorth.com" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="w-full md:flex-1 md:min-w-[20px] flex items-center justify-center company-logo-link"
+            >
+              <div className="logo-container h-10 md:h-12 w-auto md:max-w-[150px]">
+                <svg viewBox="0 0 190 50" className="h-10 md:h-12 w-auto md:max-w-[150px] logo-treated" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g clipPath="url(#clip0_413_8550)">
                   <mask id="mask0_413_8550" style={{ maskType: 'luminance' }} maskUnits="userSpaceOnUse" x="0" y="14" width="130" height="22">
                     <path d="M130 14H0V35.9027H130V14Z" fill="white"/>
@@ -907,11 +937,19 @@ export default function Page() {
                   </clipPath>
                 </defs>
               </svg>
-            </div>
+              <img src="/oaknorth-logo.svg" alt="OakNorth" className="h-10 md:h-12 w-auto md:max-w-[150px] logo-colored" />
+              </div>
+            </a>
 
             {/* Beamery Logo */}
-            <div className="hidden md:flex md:flex-1 md:min-w-[20px] md:items-center md:justify-center">
-              <svg viewBox="0 0 317.3 70" className="h-10 md:h-12 w-auto md:max-w-[150px]" fill="none">
+            <a 
+              href="https://www.beamery.com" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="hidden md:flex md:flex-1 md:min-w-[20px] md:items-center md:justify-center company-logo-link"
+            >
+              <div className="logo-container h-10 md:h-12 w-auto md:max-w-[150px]">
+                <svg viewBox="0 0 317.3 70" className="h-10 md:h-12 w-auto md:max-w-[150px] logo-treated" fill="none">
                 <style>{`.st0,.st1{fill:${DARK_TEXT}80}`}</style>
                 <g>
                   <g transform="translate(42.729 7.225)">
@@ -933,7 +971,9 @@ export default function Page() {
                   </g>
                 </g>
               </svg>
-            </div>
+              <img src="/beamery-logo.svg" alt="Beamery" className="h-10 md:h-12 w-auto md:max-w-[150px] logo-colored" />
+              </div>
+            </a>
 
           </div>
         </div>
